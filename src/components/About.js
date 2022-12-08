fe692b46e2a98548d1333a932125055d37c9f1ea
Yerassyl <63988030+EraOfCoding@users.noreply.github.com>
2022-12-09 05:51:38 +0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/About.css'
-import Boribay from '../images/Boribay.png'
+import me from '../images/me.jpg'
 import Navbar from './Navbar';
 
 class About extends React.Component {
@@ -16,10 +16,7 @@ class About extends React.Component {
                         <p>Website was created using React by Yerassyl.</p>
                         <p> If you have any questions or suggestions just DM me. @Yerassyl, @Owner guy</p>
                         <hr/>                  
-                        <div className="btns">
-                            <button onClick={() => window.open('https://discord.com/oauth2/authorize?client_id=735397931355471893&permissions=8&scope=bot')} id="invite">Invite</button>
-                            <button onClick={() => window.open('https://top.gg/bot/735397931355471893')} id="info">Info</button>
-                        </div>
+                        <img src={me}/>
                     </div>
                 </div>
             </div>
