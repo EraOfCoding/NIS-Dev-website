025c976a85bc1540b91b0b60d15ea8ec7790f2cc
Yerassyl <63988030+EraOfCoding@users.noreply.github.com>
2022-12-09 05:50:27 +0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -15,15 +15,7 @@ class About extends React.Component {
                         <h1>About website</h1>
                         <p>Website was created using React by Yerassyl.</p>
                         <p> If you have any questions or suggestions just DM me. @Yerassyl, @Owner guy</p>
-                        <hr/>
-                        <div id="boribay">
-                            <div>
-                                <h2>Don't forget to check out awesome Boribay bot!</h2>
-                                <p>It has a lot of useful features and was designed for NIS students</p>
-                            </div>
-                            <img src={Boribay}/>
-                        </div>
-                        
+                        <hr/>                  
                         <div className="btns">
                             <button onClick={() => window.open('https://discord.com/oauth2/authorize?client_id=735397931355471893&permissions=8&scope=bot')} id="invite">Invite</button>
                             <button onClick={() => window.open('https://top.gg/bot/735397931355471893')} id="info">Info</button>
